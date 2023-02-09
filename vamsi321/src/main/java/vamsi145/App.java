8fe673669120fe04a840ba4ea721eed2d3de3d8e
vamsipathala <vamsipathala11>
2023-02-09 17:36:50 -0500

    sample

---
+++ vamsi321/src/main/java/vamsi145/App.java
@@ -0,0 +1,13 @@
+package vamsi145;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
